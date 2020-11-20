68b3138244cd7c100cd65cdf2633567563242a92
Algorithm X <67506533+algorithmx51@users.noreply.github.com>
2020-11-20 20:35:52 +0200

    Update headless-neat.h

--- src/headless-neat.h
+++ src/headless-neat.h
@@ -1,11 +1,24 @@
-/**
-  *
-  * Official Copy of NEAT.h library
-  * Copywrite (c) Algorithm X, Greece
-  * Current version : v1.1
-  *
-  *
-  **/
+/*
+ *
+ *
+ * Official Copy of NEAT.h library
+ * Current version : v1.1
+ *
+ * Copyright (C) 2020  Algorithm X
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef HEADLESS_NEAT_H
 #define HEADLESS_NEAT_H
