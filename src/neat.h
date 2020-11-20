8d692df093047cb74e35a7bd94cf01eea5e75ce8
Algorithm X <67506533+algorithmx51@users.noreply.github.com>
2020-11-20 20:34:20 +0200

    Update neat.h

--- src/neat.h
+++ src/neat.h
@@ -1,11 +1,25 @@
-/**
-  *
-  * Official Copy of NEAT.h library
-  * Copywrite (c) Algorithm X, Greece
-  * Current version : v1.2
-  *
-  *
-  **/
+/*
+ *
+ *
+ * Official Copy of NEAT.h library
+ * Current version : v1.2
+ *
+ * Copyright (C) 2020  Algorithm X
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef NEAT_H
 #define NEAT_H
